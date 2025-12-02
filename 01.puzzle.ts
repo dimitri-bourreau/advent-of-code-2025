618237f44d56d2aaa30c4458980d721389c15377
Dimitri Bourreau <contact@dimitribourreau.me>
2025-12-02 15:20:19 +0100

    feat(1): complete puzzle 1-1

--- 01.puzzle.ts
+++ 01.puzzle.ts
@@ -2,20 +2,62 @@
 https://adventofcode.com/2025/day/1
  */
 
-interface Rotation {
+export interface Rotation {
   direction: "L" | "R";
   iterations: number;
 }
 
+const START_POSITION = 50;
+const NUMBERS_ON_DIAL = 99;
+
 (async () => {
   const textData = await Deno.readTextFile("./01.data.txt");
-  const rotations: Rotation[] = textData
+  const rotations: Rotation[] = extractDataFromRawText(textData);
+  const numberOfZeroSelections = findHowManyTimesZeroIsHit(
+    rotations,
+    START_POSITION,
+    NUMBERS_ON_DIAL
+  );
+  console.log(`Zero has been hit ${numberOfZeroSelections} times.`);
+})();
+
+export function extractDataFromRawText(rawText: string): Rotation[] {
+  return rawText
     .split("\n")
     .filter((line) => line.trim() !== "")
     .map((line) => ({
       direction: line.slice(0, 1) as Rotation["direction"],
       iterations: parseInt(line.slice(1), 10),
     }));
+}
 
-  console.log(rotations);
-})();
+export function findHowManyTimesZeroIsHit(
+  rotations: Rotation[],
+  startPosition: number,
+  numbersOnDial: number
+): number {
+  let position = startPosition;
+  let numberOfZeroSelections = 0;
+
+  for (const { direction, iterations } of rotations) {
+    if (direction === "L") {
+      position -= iterations;
+    } else {
+      position += iterations;
+    }
+    position = handlePositionIfOutsideRange(position, numbersOnDial);
+    if (position === 0) numberOfZeroSelections++;
+  }
+
+  return numberOfZeroSelections;
+}
+
+export function handlePositionIfOutsideRange(
+  position: number,
+  numbersOnDial: number
+): number {
+  const moduloPosition = position % (numbersOnDial + 1);
+  if (moduloPosition < 0) return moduloPosition + numbersOnDial + 1;
+  if (numbersOnDial < moduloPosition) return moduloPosition - numbersOnDial + 1;
+  return moduloPosition;
+}
---
+++ 01.test.ts
@@ -0,0 +1,118 @@
+import { describe, it } from "jsr:@std/testing/bdd";
+import { expect } from "jsr:@std/expect";
+import {
+  extractDataFromRawText,
+  findHowManyTimesZeroIsHit,
+  handlePositionIfOutsideRange,
+  Rotation,
+} from "./01.puzzle.ts";
+
+describe("extractDataFromRawText", () => {
+  it("should extract three lines", () => {
+    const rotations: Rotation[] = extractDataFromRawText("L50\nR50\nL16");
+    expect(rotations.length).toBe(3);
+  });
+
+  it("should return -50 +18 -20", () => {
+    const rotations: Rotation[] = extractDataFromRawText("L50\nR18\nL20");
+    const expected: Rotation[] = [
+      {
+        direction: "L",
+        iterations: 50,
+      },
+      {
+        direction: "R",
+        iterations: 18,
+      },
+      {
+        direction: "L",
+        iterations: 20,
+      },
+    ];
+    expect(rotations).toEqual(expected);
+  });
+});
+
+describe("findHowManyTimesZeroIsHit", () => {
+  it("should return 1 hit for default 50 and 99 numbers: -50", () => {
+    const rotations: Rotation[] = [{ direction: "L", iterations: 50 }];
+    const numberOfZeroHits = findHowManyTimesZeroIsHit(rotations, 50, 99);
+    expect(numberOfZeroHits).toBe(1);
+  });
+
+  it("should return 0 hits for default 50 and 99 numbers: -99 +99", () => {
+    const rotations: Rotation[] = [
+      { direction: "L", iterations: 99 },
+      { direction: "R", iterations: 99 },
+    ];
+    const numberOfZeroHits = findHowManyTimesZeroIsHit(rotations, 50, 99);
+    expect(numberOfZeroHits).toBe(0);
+  });
+
+  it("should return 3 hits for default 50 and 99 numbers: -50 -100 +100", () => {
+    const rotations: Rotation[] = [
+      { direction: "L", iterations: 50 },
+      { direction: "L", iterations: 100 },
+      { direction: "R", iterations: 100 },
+    ];
+    const numberOfZeroHits = findHowManyTimesZeroIsHit(rotations, 50, 99);
+    expect(numberOfZeroHits).toBe(3);
+  });
+});
+
+describe("handlePositionIfOutsideRange()", () => {
+  it("should let position at 0 when given position 0 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(0, 99);
+    expect(newPosition).toBe(0);
+  });
+
+  it("should return position 82 when given position -18 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(-18, 99);
+    expect(newPosition).toBe(82);
+  });
+
+  it("should return position 52 when given position 52 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(52, 99);
+    expect(newPosition).toBe(52);
+  });
+
+  it("should return position 99 when given position -1 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(-1, 99);
+    expect(newPosition).toBe(99);
+  });
+
+  it("should return position 1 when given position 1 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(1, 99);
+    expect(newPosition).toBe(1);
+  });
+
+  it("should return position 0 when given position 100 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(100, 99);
+    expect(newPosition).toBe(0);
+  });
+
+  it("should return position 1 when given position 101 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(101, 99);
+    expect(newPosition).toBe(1);
+  });
+
+  it("should return position 10 when given position 110 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(110, 99);
+    expect(newPosition).toBe(10);
+  });
+
+  it("should return position 95 when given position -5 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(110, 99);
+    expect(newPosition).toBe(10);
+  });
+
+  it("should return position 60 when given position 560 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(560, 99);
+    expect(newPosition).toBe(60);
+  });
+
+  it("should return position 66 when given position -234 and 99 numbers on dial", () => {
+    const newPosition = handlePositionIfOutsideRange(-234, 99);
+    expect(newPosition).toBe(66);
+  });
+});
