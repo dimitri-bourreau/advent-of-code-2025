615223b5ce861f1a7fabc08d18361fbbf0dd86d2
Dimitri Bourreau <contact@dimitribourreau.me>
2025-12-04 00:56:49 +0100

    feat(1): wrap test in PUZZLE 1 & test output for puzzle 1-2

--- 01-2.puzzle.ts
+++ 01-2.puzzle.ts
@@ -7,7 +7,11 @@ import { Rotation } from "./01.types.ts";
 const START_POSITION = 50;
 const NUMBERS_ON_DIAL = 99;
 
-(async () => {
+(() => {
+  void puzzle1Part2();
+})();
+
+export async function puzzle1Part2() {
   const textData = await Deno.readTextFile("./01.data.txt");
   const rotations: Rotation[] = extractDataFromRawText(textData);
   const numberOfZeroSelections = findHowManyTimesZeroIsHit(
@@ -17,4 +21,5 @@ const NUMBERS_ON_DIAL = 99;
     true
   );
   console.log(`Zero has been hit and passed ${numberOfZeroSelections} times.`);
-})();
+  return numberOfZeroSelections;
+}
