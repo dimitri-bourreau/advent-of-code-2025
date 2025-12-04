ac4c7d249ae6c7dd620177e26cafebd776d63144
Dimitri Bourreau <contact@dimitribourreau.me>
2025-12-04 02:23:07 +0100

    refactor(2): extract iteration logic for perf and readability

---
+++ 02-2.puzzle.ts
@@ -0,0 +1,13 @@
+import { sumInvalidIDs, parseRanges } from "./02.functions.ts";
+
+(() => {
+  void puzzle2Part2();
+})();
+
+export async function puzzle2Part2() {
+  const textData = await Deno.readTextFile("./02.data.txt");
+  const ranges = parseRanges(textData);
+  const invalidIDsCount = sumInvalidIDs(ranges, true);
+  console.log(`Invalid IDs' sum: ${invalidIDsCount}`);
+  return invalidIDsCount;
+}
--- 02.functions.ts
+++ 02.functions.ts
@@ -4,8 +4,13 @@ export const parseRanges = (rawRanges: string): Range[] => {
   return rawRanges.trim().replace("\n", "").split(",").map(parseRange);
 };
 
-export const sumInvalidIDs = (ranges: Range[]): number => {
-  const invalidIDs: number[][] = ranges.map(getInvalidIDs);
+export const sumInvalidIDs = (
+  ranges: Range[],
+  countIDsWithSeveralRepetitions?: boolean
+): number => {
+  const invalidIDs: number[][] = ranges.map((range) =>
+    getInvalidIDs(range, countIDsWithSeveralRepetitions)
+  );
   return sumAllNumbers(invalidIDs.map(sumAllNumbers));
 };
 
@@ -17,10 +22,21 @@ function parseRange(range: string): Range {
   };
 }
 
-export function getInvalidIDs({ start, end }: Range): number[] {
+export function getInvalidIDs(
+  { start, end }: Range,
+  countIDsWithSeveralRepetitions?: boolean
+): number[] {
   const invalidIds: number[] = [];
   for (let i = start; i <= end; i++) {
-    if (numberIsMadeOfOneRepetition(i)) invalidIds.push(i);
+    const id = i.toString();
+    const idLengthIsEven = id.length % 2 === 0;
+    if (idLengthIsEven && id[0] !== "0") {
+      if (countIDsWithSeveralRepetitions) {
+        if (idIsMadeOfRepetitions(id)) invalidIds.push(i);
+      } else {
+        if (idIsMadeOfOneRepetition(id)) invalidIds.push(i);
+      }
+    }
   }
   return invalidIds;
 }
@@ -29,15 +45,13 @@ function sumAllNumbers(numbers: number[]): number {
   return numbers.reduce((a, b) => (a += b), 0);
 }
 
-function numberIsMadeOfOneRepetition(number: number): boolean {
-  const rangeAsString = number.toString();
-  const idLengthIsEven = rangeAsString.length % 2 === 0;
-  if (rangeAsString[0] === "0") return true;
-  if (idLengthIsEven) {
-    const halfIndex = rangeAsString.length / 2;
-    const firstHalf = rangeAsString.slice(0, halfIndex);
-    const lastHalf = rangeAsString.slice(halfIndex);
-    return firstHalf === lastHalf;
-  }
-  return false;
+function idIsMadeOfOneRepetition(id: string): boolean {
+  const halfIndex = id.length / 2;
+  const firstHalf = id.slice(0, halfIndex);
+  const lastHalf = id.slice(halfIndex);
+  return firstHalf === lastHalf;
+}
+
+function idIsMadeOfRepetitions(id: string): boolean {
+  return true;
 }
